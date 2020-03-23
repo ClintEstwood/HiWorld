cce4810b8231a6061e12dd021913bb8d32c75180
Sergey Trusilnikov <strusilnikov@gmail.com>
2020-03-23 14:23:24 +0100

    cycle added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hiii");
+        for (int i = 0; i<=5; i++){
+            System.out.println("Hiii");
+        }
     }
 }
